3d2409f53e0a414ea53d3d7b7219e6d0db12c6e2
blackdiamond7804 <pacampoverde1@espe.edu.ec>
2026-06-20 23:02:25 -0500

    Agregar sanitización y documentación Swagger para API de vehículos

--- vehiculos/src/vehiculos/dto/update-vehiculo.dto.ts
+++ vehiculos/src/vehiculos/dto/update-vehiculo.dto.ts
@@ -1,6 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateVehiculoDto } from './create-vehiculo.dto';
 
-export class UpdateVehiculoDto extends PartialType(CreateVehiculoDto) {
-    
+export class UpdateVehiculoDto extends PartialType(CreateVehiculoDto) {   
 }
